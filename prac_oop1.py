48c1354916cb994e5c9d218214a9b6c6f139502d
khanbaba-ahamaliiev <khanbaba.ahamaliiev1@gmail.com>
2026-03-26 23:07:47 +0400

    Практичне завдання

--- prac_oop1.py
+++ prac_oop1.py
@@ -83,3 +83,111 @@ car1.show_info()
 car2 = Car("mersedes", 2017)
 car2.move()
 car2.show_info()
+
+
+# Додаткове завдання 1
+
+class BankCard:
+    def __init__(self, owner, balance, pin, daily_limit, withdrawn_today):
+        self.owner = owner
+        self.balance = balance
+        self.pin = pin
+        self.daily_limit = daily_limit
+        self.withdrawn_today = withdrawn_today
+
+    def authorization(self, your_pin):
+
+        if your_pin == str(self.pin):
+            print("your pin is valid, welcome")
+            return True
+
+
+        else:
+            print("your pin is not valid")
+            return False
+
+    def deposit(self, amount):
+        if not self.authorization:
+            print("authorization failed")
+        else:
+            print("authorization successful")
+
+        if amount > 0:
+            self.balance += amount
+        else:
+            print("amount cannot be negative")
+
+    def withdraw(self, amount):
+        if not self.authorization:
+            print("authorization failed")
+        else:
+            print("authorization successful")
+
+        if amount >= self.daily_limit:
+            print("you have daily limit")
+        else:
+            if self.balance >= amount:
+                self.balance -= amount
+                self.withdrawn_today += amount
+
+    def reset_daily_limit(self):
+        new_day = True
+        if new_day:
+            self.daily_limit = 0
+
+    def show_info(self):
+        print(f"{self.owner}'s balance is {self.balance}, pin: {self.pin}, \ndaily limit: {self.daily_limit}, withdrawn_today: {self.withdrawn_today} ")
+user = BankCard("John", 1000, 1234, 500, 0)
+
+user.authorization(1234)
+user.deposit(500)
+user.withdraw(200)
+user.show_info()
+
+
+class Car:
+    def __init__(self, brand, year, fuel, fuel_consumption, mileage, is_engine_on=False):
+        self.brand = brand
+        self.year = year
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+        self.mileage = mileage
+        self.is_engine_on = is_engine_on
+
+    def start_engine(self):
+        if not self.is_engine_on:
+            self.is_engine_on = True
+            print(f"{self.brand} {self.year} is ready")
+        else:
+            print(f"{self.brand} {self.year} is already ready")
+
+    def stop_engine(self):
+        if self.is_engine_on:
+            self.is_engine_on = False
+            print(f"{self.brand} {self.year} is stopped")
+        else:
+            print(f"{self.brand} {self.year} is already stopped")
+
+    def add_fuel(self, litres):
+        self.fuel += litres
+        print(f"{self.brand} {self.year} is added {litres} litres fuel")
+
+    def moving(self, distance):
+        if not self.is_engine_on:
+            print(f"{self.brand} {self.year} need to start engine")
+        else:
+            if distance < (self.fuel/self.fuel_consumption):
+                self.mileage += distance
+                self.fuel -= distance * self.fuel_consumption
+            else:
+                print(f"{self.brand} {self.year} not enough fuel")
+                print(f"you can move {self.fuel/self.fuel_consumption} km")
+
+    def show_info(self):
+        print(f"{self.brand} {self.year}, mileage: {self.mileage}, fuel: {self.fuel}, is_engine_on: {self.is_engine_on}")
+
+car1 = Car("toyota", 2021, 100, 8, 1230)
+car1.start_engine()
+car1.add_fuel(12)
+car1.moving(5)
+car1.show_info()
