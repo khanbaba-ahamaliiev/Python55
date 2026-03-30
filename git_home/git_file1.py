77f93322a01f787a18d63acb366e88791d268f59
khanbaba-ahamaliiev <khanbaba.ahamaliiev1@gmail.com>
2026-03-30 21:35:45 +0400

    makes error 2

--- git_home/git_file1.py
+++ git_home/git_file1.py
@@ -2,6 +2,8 @@ text = ("I enjoy learning new things every day.\n"
         "It helps me grow and become better.\n"
         "Even small steps can lead to big успехи in the future.")
 
-
+ i = 1+1+1+1
+ print(i * 700)
+ text = '12345'
 
 print(text)
