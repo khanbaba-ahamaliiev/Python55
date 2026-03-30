c5b1ebc013845d015e2e0989a06d0e98acf9495d
khanbaba-ahamaliiev <khanbaba.ahamaliiev1@gmail.com>
2026-03-30 21:28:59 +0400

    git homework created newbranch with files

--- git_home/git_file1.py
+++ git_home/git_file1.py
@@ -2,4 +2,6 @@ text = ("I enjoy learning new things every day.\n"
         "It helps me grow and become better.\n"
         "Even small steps can lead to big успехи in the future.")
 
+
+
 print(text)
