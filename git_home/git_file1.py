568dfe396c30c1d0f002e67316b32296422ff140
khanbaba-ahamaliiev <khanbaba.ahamaliiev1@gmail.com>
2026-03-30 21:34:19 +0400

    makes error

--- git_home/git_file1.py
+++ git_home/git_file1.py
@@ -2,6 +2,10 @@ text = ("I enjoy learning new things every day.\n"
         "It helps me grow and become better.\n"
         "Even small steps can lead to big успехи in the future.")
 
-
+for letter in text:
+        print(letter)
+        letter = letter.lower()
+        letter = letter.upper()
+        print(letter)
 
 print(text)
