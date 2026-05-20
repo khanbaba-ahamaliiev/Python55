35e8962d466b37ed4e3f7227c4972d7f83fc4f4d
khanbaba-ahamaliiev <khanbaba.ahamaliiev1@gmail.com>
2026-05-20 17:27:57 +0400

    dd

--- docker_homework3/settings.py
+++ docker_homework3/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     host: str = "0.0.0.0"
     port: int = 8080
-    max_films: int | None = None
+    MAX_FILMS: int | None = None
     data_file_path: str = "./films.json"
 
     model_config = SettingsConfigDict(
